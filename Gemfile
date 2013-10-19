d208c85385188c87fd8a13689ff9a64b59f900ef
Vitor Baptista <vitor@vitorbaptista.com>
2013-10-19 19:14:58 -0300

    Configura VCR para otimizar os testes

--- Gemfile
+++ Gemfile
@@ -8,5 +8,7 @@ group :development do
   gem "rdoc", "~> 3.12"
   gem "bundler", "~> 1.0"
   gem "jeweler", "~> 1.8.7"
+  gem "vcr"
+  gem "webmock", "~> 1.13.0"
   gem "pry"
 end
--- test/test_console.rb
+++ test/test_console.rb
@@ -2,14 +2,18 @@ require 'helper'
 
 class TestConsole < Test::Unit::TestCase
   should 'login successfully when called with valid credentials' do
-    assert_nothing_raised ESIC::LoginFailedException do
-      ESIC::Crawler.new('vitorbaptista', 'the-real-password')
+    VCR.use_cassette('console_login_successful') do
+      assert_nothing_raised ESIC::LoginFailedException do
+        ESIC::Crawler.new('vitorbaptista', 'correct-password')
+      end
     end
   end
 
   should 'fails login when called with invalid credentials' do
-    assert_raise ESIC::LoginFailedException do
-      ESIC::Crawler.new('vitorbaptista', 'wrong-password')
+    VCR.use_cassette('console_login_failure') do
+      assert_raise ESIC::LoginFailedException do
+        ESIC::Crawler.new('vitorbaptista', 'wrong-password')
+      end
     end
   end
 end
